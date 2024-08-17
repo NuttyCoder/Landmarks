fe24d72b2ff90445c8dea1dd0af4995831d01c79
NuttyCoder <79060006+NuttyCoder@users.noreply.github.com>
2024-08-17 07:22:05 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Heather Scheer on 8/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("this is the new landmarks!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
